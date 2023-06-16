1d718842e4008c99b9d3bf84ec494a500dcfffb8
Igor <igor1788@gmail.com>
2023-06-16 12:04:01 -0300

    Project start

---
+++ Frontend/src/components/Form.tsx
@@ -0,0 +1,64 @@
+import React, { useState } from 'react';
+import './Form.css';
+
+const Form = () => {
+  const [data, setData] = useState({
+    segmento: '',
+    produto: '',
+    descricao: '',
+    preco: '',
+    cupom: '',
+    link: '',
+    imagem: '',
+  });
+
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setData({
+      ...data,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  
+    try {
+      const response = await fetch('http://localhost:3001/cadastro', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      });
+  
+      const responseData = await response.json();
+  
+      if (response.ok) {
+        console.log('Data registered successfully');
+        setErrorMessage('');
+      } else {
+        setErrorMessage(responseData.message || 'Error registering data');
+      }
+    } catch (error) {
+      setErrorMessage('An error occurred while registering the data.');
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input name='segmento' value={data.segmento} onChange={handleChange} placeholder='Segmento' required />
+      <input name='produto' value={data.produto} onChange={handleChange} placeholder='Produto' required />
+      <input name='descricao' value={data.descricao} onChange={handleChange} placeholder='Descrição' required />
+      <input name='preco' value={data.preco} onChange={handleChange} placeholder='Preço' required />
+      <input name='cupom' value={data.cupom} onChange={handleChange} placeholder='Cupom' required />
+      <input name='link' value={data.link} onChange={handleChange} placeholder='Link' required />
+      <input name='imagem' value={data.imagem} onChange={handleChange} placeholder='Imagem' required />
+      <button type='submit'>Enviar</button>
+      {errorMessage && <p className="error">{errorMessage}</p>}
+    </form>
+  );
+};
+
+export default Form;
