ddbfa9d411567c1a173d466b415968597a2ca6eb
Igor <igor1788@gmail.com>
2023-06-20 09:12:04 -0300

    New Docker container install

--- app.ts
+++ app.ts
@@ -42,4 +42,6 @@ app.post('/cadastro', async (req, res) => {
 });
 
 const port: number = Number(process.env.PORT) || 3000;
-app.listen(port, () => console.log(`Servidor rodando na porta ${port}`));
+app.listen(port, '0.0.0.0', () => {
+    console.log(`Server started at http://localhost:${port}`);
+  });
